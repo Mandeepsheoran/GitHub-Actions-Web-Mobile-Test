56de1635df5e312f742ad6ad873c2531ad845387
Mandeepsheoran <70449523+Mandeepsheoran@users.noreply.github.com>
2023-10-04 19:32:41 +0400

    Update IosTest.java

--- src/test/java/org/mobile/test/IosTest.java
+++ src/test/java/org/mobile/test/IosTest.java
@@ -1,9 +1,9 @@
 package org.mobile.test;
 
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
@@ -21,7 +21,7 @@ public class IosTest {
 	        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPod touch (7th generation)");
 	        //capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/DailyCheck.zip");
 	        capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"/TestApp-iphonesimulator.app");
-	        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),
+	        WebDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
 	                capabilities);
 	    }
 }
